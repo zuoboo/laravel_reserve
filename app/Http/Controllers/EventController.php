d40f9e8839e640fd109063799e5406199a9d2425
github zuoboo <nsnxn1022@gmail.com>
2022-09-14 21:27:32 +0900

    event store

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,8 @@ use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+use App\Services\EventService;
 
 class EventController extends Controller
 {
@@ -17,8 +19,8 @@ class EventController extends Controller
     public function index()
     {
         $events = DB::table('events')
-        ->orderBy('start_date', 'asc')
-        ->paginate(10);
+            ->orderBy('start_date', 'asc')
+            ->paginate(10);
         return view('manager.events.index', compact('events'));
     }
 
@@ -40,7 +42,30 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        //
+
+        $check = EventService::checkEventDuplication(
+            $request['event_date'], $request['start_time'], $request['end_time']);
+
+        if ($check) {
+            session()->flash('status', 'この時間帯は既に他の予約が存在します。');
+            return view('manager.events.create');
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'],$request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'],$request['end_time']);
+
+
+        Event::create([
+            'name' => $request['event_name'],
+            'information' => $request['information'],
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'max_people' => $request['max_people'],
+            'is_visible' => $request['is_visible']
+        ]);
+
+        session()->flash('status', '登録OKです');
+        return to_route('events.index');
     }
 
     /**
