7bd335ce074641dad8607b2280b0bd020ed5aa99
github zuoboo <nsnxn1022@gmail.com>
2022-09-14 15:47:54 +0900

    gate

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,21 @@ Route::middleware([
     })->name('dashboard');
 });
 
+Route::prefix('manager')
+->middleware('can:manager-higher')
+->group(function(){
+    Route::get('index', function(){
+        dd('manager');
+    });
+});
+
+Route::middleware('can:user-higher')
+->group(function(){
+    Route::get('index', function(){
+        dd('user');
+    });
+});
+
 Route::controller(LivewireTestController::class)
 ->prefix('livewire-test')->name('livewire-test.')->group(function(){
     Route::get('index', 'index')->name('index');
