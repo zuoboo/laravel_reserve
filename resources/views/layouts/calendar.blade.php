7d8c51650d8ce00156cf56e453afeee959ba046f
github zuoboo <nsnxn1022@gmail.com>
2022-11-09 23:49:53 +0900

    hidden

--- resources/views/layouts/calendar.blade.php
+++ resources/views/layouts/calendar.blade.php
@@ -25,7 +25,7 @@
 
     <div class="min-h-screen bg-gray-100">
         @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            <div class="fixed top-0 right-0 px-6 py-4 sm:block">
                 @auth
                     <a href="{{ url('/dashboard') }}"
                         class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
